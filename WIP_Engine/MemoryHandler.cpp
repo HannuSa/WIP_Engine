5e10a87b7fb7f74309291066106dfccb0ac1a1c6
nicosebi <TTI13SNicoA@kamk.fi>
2014-10-31 12:44:22 +0200

    Addes setter to memory handler

--- WIP_Engine/MemoryHandler.cpp
+++ WIP_Engine/MemoryHandler.cpp
@@ -3,7 +3,8 @@
 
 MemoryHandler::MemoryHandler()
 {
-	arraysize = 0;
+	vertexsize = 0;
+	valueNum = 7;
 }
 
 
@@ -11,11 +12,15 @@ MemoryHandler::~MemoryHandler()
 {
 }
 
-void MemoryHandler::Allocate(unsigned int _arraysize)
+void MemoryHandler::Allocate(unsigned int _vertexsize, unsigned int _indexsize)
 {
-	vertexArray = new GLfloat[_arraysize];
-	arraysize = _arraysize;
-	Debug::Dvalue("", arraysize);
+	vertexArray = new GLfloat[_vertexsize];
+	vertexsize = _vertexsize;
+	Debug::Dvalue("", vertexsize);
+
+	indexArray = new GLuint[_indexsize];
+	indexsize = _indexsize;
+	Debug::Dvalue("", indexsize);
 
 	InitArrays();
 }
@@ -23,22 +28,61 @@ void MemoryHandler::Allocate(unsigned int _arraysize)
 
 void MemoryHandler::DeAllocate()
 {
-	if (arraysize != 0)
+	if (vertexsize != 0)
 	{
 		delete[] vertexArray;		
+	}
+	if (indexsize != 0)
+	{
+		delete[] indexArray;
 	}
 }
 
 void MemoryHandler::InitArrays()
 {
-	for (int i = 0; i < arraysize; i++)
+	for (int i = 0; i < vertexsize; i++)
 	{
-		vertexArray[i] = 0.0f;
-	}
-	
+		vertexArray[i] = 1.0f;
+	}	
 }
 
-size_t MemoryHandler::getVarraySize()
+void MemoryHandler::setPos(unsigned int _vertex, GLfloat _x, GLfloat _y)
 {
-	return sizeof(vertexArray)*arraysize;
+	if (_vertex < vertexsize)
+	{
+		vertexArray[_vertex * valueNum] = _x;
+		vertexArray[_vertex * valueNum + 1] = _y;
+	}
+	Debug::Message(_vertex < vertexsize, "setPos: vertexArray out of pounds");
+}
+
+void MemoryHandler::setColor(unsigned int _vertex, GLfloat _x, GLfloat _y, GLfloat _z)
+{
+	if (_vertex < vertexsize)
+	{
+		vertexArray[_vertex * valueNum + 2] = _x;
+		vertexArray[_vertex * valueNum + 3] = _y;
+		vertexArray[_vertex * valueNum + 4] = _z;
+	}
+	Debug::Message(_vertex < vertexsize, "setColor: vertexArray out of pounds");
+}
+
+void MemoryHandler::setTexture(unsigned int _vertex, GLfloat _x, GLfloat _y)
+{
+	if (_vertex < vertexsize)
+	{
+		vertexArray[_vertex * valueNum + 5] = _x;
+		vertexArray[_vertex * valueNum + 6] = _y;
+	}
+	Debug::Message(_vertex < vertexsize, "setTexture: vertexArray out of pounds");
+}
+
+size_t MemoryHandler::getVertexSize()
+{
+	return sizeof(vertexArray)*vertexsize;
+}
+
+size_t MemoryHandler::getIndexSize()
+{
+	return sizeof(indexArray)*indexsize;
 }
--- WIP_Engine/MemoryHandler.h
+++ WIP_Engine/MemoryHandler.h
@@ -14,14 +14,21 @@ public:
 	MemoryHandler();
 	~MemoryHandler();
 
-	void Allocate(unsigned int _arraysize);//Allocates the arrays
+	void Allocate(unsigned int _vertexsize, unsigned int _indexsize);//Allocates the arrays
 	void DeAllocate();
-	size_t getVarraySize();//Returns the size of of vertexArray
+	size_t getVertexSize();//Returns the size of of vertexArray
+	size_t getIndexSize();//Returns the size of of vertexArray
+	
+	GLfloat *vertexArray;
+	GLuint *indexArray;
+
+	void setPos(unsigned int _vertex, GLfloat _x, GLfloat _y);//Setter for position
+	void setColor(unsigned int _vertex, GLfloat _x, GLfloat _y, GLfloat _z);//Setter for color
+	void setTexture(unsigned int _vertex, GLfloat _x, GLfloat _y);//Setter for texture position
+
+	private:
+	unsigned int vertexsize, indexsize, valueNum; //vertexsize + indexsize = array size, valueNum = values in single vertex, 
 
 	void InitArrays();//Initializes arrays to 1.0f (white color)
-
-//private:
-	GLfloat *vertexArray;
-	unsigned int arraysize;
 };
 

a7a76bab20694c4d9bad76dc91dafc3db7328885
Heimiric <alluoma@outlook.com>
2014-12-12 12:21:04 +0200

    Demo
    Stuff spins and scales and moves

--- WIP_Engine/Main.cpp
+++ WIP_Engine/Main.cpp
@@ -7,62 +7,16 @@
 #include "ResourceManager.h"
 #include "Texture.h"
 #include "MemoryHandler.h"
-
-static const GLfloat VERTEX_DATA[] =	//add texture coords
-{
-	//1st vertex
-	600, 100,//Positio
-	0.75f, 0.0f, 0.0f,//color
-	1.0f, 0.0f,//texture
-
-	//2nd vertex
-	200, 500,
-	0.0f, 0.0f, 0.75f,
-	0.0f, 0.0f,
-
-	//3rd vertex
-	1000, 500,
-	0.0f, 0.0f, 0.0f,
-	0.5f, 1.0f
-};
-
-static const GLuint INDEX_DATA[] =
-{
-	0, 1, 2
-};
+#include "Input.h"
+#include "Game_Timer_Random.h"
 
 int main()
 {
 
-	//MemoryHandler m;
-	//m.Allocate(3 * 7, 3);
-	//
-	////1st vertex
-	//m.setPos(0, 600, 100);
-	//m.setColor(0, 1.0f, 1.0f, 1.0f);
-	//m.setTexture(0, 1.0f, 0.0f);
-
-	////2nd vertex
-	//m.setPos(1, 200, 500);
-	//m.setColor(1, 1.0f, 1.0f, 1.0f);
-	//m.setTexture(1, 0.0f, 0.0f);
-
-	////3rd vertex
-	//m.setPos(2, 1000, 500);
-	//m.setColor(2, 1.0f, 1.0f, 1.00f);
-	//m.setTexture(2, 0.5f, 1.0f);
-
-	////Index array
-	//m.indexArray[0] = 0;
-	//m.indexArray[1] = 1;
-	//m.indexArray[2] = 2;
-
 	Window window(1200, 600, "This is window!");
-	Render render;
+	Render render(1200, 600);
 	
 	render.EnableAttributeArray();
-
-	/*render.CreateBuffers(m.getVertexSize(), m.getIndexSize(), m.vertexArray, m.indexArray);*/
 
 	window.SetClearColor(100, 50, 150);
 
@@ -72,58 +26,132 @@ int main()
 
 	ResourceManager* res = ResourceManager::Create();
 
-	Texture* texture;
-	texture = &res->LoadTextureFromFile("ColoroftheRainbow.png");
-	Texture* texture2 = &res->LoadTextureFromFile("Test.png");
+	Texture* card;
+	card = &res->LoadTextureFromFile("Test.png");
+	Texture* bg = &res->LoadTextureFromFile("Background.png");
+	Texture bulletTex = res->LoadTextureFromFile("Bullet.png");
+	Texture chipTex = res->LoadTextureFromFile("suitsChip.png");
 
-	Sprite test;
-	Sprite test2, test3;
-	test.SetTexture(*texture2);
-	test2.SetTexture(*texture);
-	test.SetPosition(700, 500);
-	test2.SetPosition(200, 200);
-	test3.SetTexture(*texture);
-	test3.SetPosition(400, 400);
-	test.SetSize(200, 291);
-	test2.Scale(1.0f);
-	test.Rotate(30.0f);
+	Sprite Background, Card;
+	Background.SetTexture(*bg);
+	Background.Scale(1.5);
+	Background.SetPosition(-800, -600);
+	Card.SetTexture(*card);
+	Card.SetPosition(400, 300);
 
-	std::vector<Sprite> megatest;
+	std::vector<Sprite> bullets, chips;
 
-	for (unsigned int i = 0; i < 5000; i++)
+	int r = 0, c = 0;
+	for (int i = 0; i < 12; i++)
 	{
-		megatest.push_back(test);
+		r = i % 4;
+		c = i % 3;
+		Sprite temp;
+		temp.SetTexture(chipTex);
+		temp.SetPosition(r*350, c*225);
+		chips.push_back(temp);
 	}
 
 	window.SetClearColor(100, 50, 150);
-	float x = 100, y = 100;
 	float rotation = 0;
-	int timer = 0;
+	float cardSpeed = 10.0f, rotationSpeed = 2.0f, bulletSpeed = 15.0f;
+	float chipScale = 1.0f;
+	bool grow = true;
 
-	test2.Scale(0.25f);
+	float timer = 0;
 
 	while (window.IsOpen())
 	{
-		window.Clear();
-		//render.BeginSpriteBatch();
-		//render.DrawSprite(miuOmaSprite);
-		//render.DrawSprite(jonkuToisenSprite);
-		//render.EndSpriteBatch();
-	/*	render.DebugDrawStuff(&texture);*/
 		timer++;
-		rotation++;
-		//test.SetPosition(x, y);
-		render.DrawSprite(test);	
-		render.DrawSprite(test2);
-		render.DrawSprite(test3);
-		for (int i = 0; i < megatest.size(); i++)
+
+		if (Input::Hold(Button::W))
 		{
-			render.DrawSprite(megatest[i]);
-			megatest[i].Rotate(rotation*i);
+			Card.SetPosition(Card.GetX(), Card.GetY()-cardSpeed);
 		}
-		test2.Rotate(rotation);
-		test.Rotate(rotation);
-		test3.Rotate(rotation);
+		if (Input::Hold(Button::A))
+		{
+			Card.SetPosition(Card.GetX()-cardSpeed, Card.GetY());
+		}
+		if (Input::Hold(Button::S))
+		{
+			Card.SetPosition(Card.GetX(), Card.GetY()+cardSpeed);
+		}
+		if (Input::Hold(Button::D))
+		{
+			Card.SetPosition(Card.GetX()+cardSpeed, Card.GetY());
+		}
+
+
+		if (Input::Hold(Button::UP))
+		{
+			rotation += rotationSpeed;
+		}
+		if (Input::Hold(Button::LEFT))
+		{
+			rotation -= rotationSpeed;
+		}
+		if (Input::Hold(Button::DOWN))
+		{
+			rotation -= rotationSpeed;
+		}
+		if (Input::Hold(Button::RIGHT))
+		{
+			rotation += rotationSpeed;
+		}
+
+		Card.Rotate(rotation);
+		Background.Rotate(-rotation);
+
+		if (Input::Press(Button::SPACE))
+		{
+			Sprite temp;
+			temp.SetTexture(bulletTex);
+			temp.Rotate(rotation);
+			temp.SetPosition((Card.GetX()+(Card.GetWidth()*0.5 - temp.GetWidth()*0.5)), (Card.GetY()+(Card.GetHeight()*0.5 - temp.GetHeight()*0.5)));
+			bullets.push_back(temp);
+		}
+
+		if (chipScale >= 1.0251f)
+			grow = false;
+		if (chipScale <= 0.975f)
+			grow = true;
+
+		if (grow)
+		{
+			chipScale += 0.001f;
+		}
+		else if (!grow)
+		{
+			chipScale -= 0.001f;
+		}
+		for (int i = 0; i < chips.size(); i++)
+		{
+			chips[i].Scale(chipScale);
+		}
+
+		for (int i = 0; i < bullets.size(); i++)
+		{
+			float tempX = bullets[i].GetX(), tempY = bullets[i].GetY();
+			float tempRot = glm::radians(bullets[i].GetRotation());
+			tempX += bulletSpeed*glm::sin(tempRot);
+			tempY -= bulletSpeed*glm::cos(tempRot);
+			bullets[i].SetPosition(tempX, tempY);
+		}
+
+		window.Clear();
+		
+		render.DrawSprite(Background);	
+		for (int i = 0; i < chips.size(); i++)
+		{
+			render.DrawSprite(chips[i]);
+		}
+		render.DrawSprite(Card);
+		for (int i = 0; i < bullets.size(); i++)
+		{
+			render.DrawSprite(bullets[i]);
+		}
+
+	
 
 		window.Display();
 	}
